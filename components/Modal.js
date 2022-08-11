5be6fe5b11afdf869e3e387c3fdc8e4963a302ba
FediRouatbi <fedirouatbi@gmail.com>
2022-08-11 19:29:12 +0100

    fix :modal view

--- components/Modal.js
+++ components/Modal.js
@@ -2,6 +2,10 @@ import React from "react";
 import Portal from "./Portal";
 const Modal = ({ closeModal }) => {
   document.body.style.overflow = "hidden";
+  const closeShowModal = () => {
+    document.body.style.overflow = "visible";
+    closeModal();
+  };
   return (
     <>
       <h1 className=" fixed w-full h-screen overflow-hidden bg-black/70 backdrop-blur-sm inset-0 z-10  "></h1>
@@ -27,7 +31,7 @@ const Modal = ({ closeModal }) => {
               </p>
               <div className="flex items-center justify-between gap-4 w-full mt-8">
                 <button
-                  onClick={closeModal}
+                  onClick={closeShowModal}
                   type="button"
                   className="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
                 >
